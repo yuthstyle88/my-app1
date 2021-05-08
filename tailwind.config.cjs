f88a8c4cb81e8ec5611ae678f01cad8a4adb9f24
the style88 <the_style88@icounds.com>
2021-05-08 17:17:10 +0700

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+const { tailwindExtractor } = require("tailwindcss/lib/lib/purgeUnusedStyles");
+
+module.exports = {
+	mode: "all",
+	purge: {
+		content: [
+			"./src/**/*.svelte",
+		],
+		options: {
+			defaultExtractor: (content) => [
+				// If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
+				...tailwindExtractor(content),
+				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
+				...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+			],
+			keyframes: true,
+			fontFace: true,
+		},
+		enabled: true
+	},
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
